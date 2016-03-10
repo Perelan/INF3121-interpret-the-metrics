3e2ea63056176191e468ca6e41121acac0bb58f4
Perelan <sinriat@gmail.com>
2016-03-10 17:55:19 +0100

    removed whitespaces and minor changes

--- MineField.java
+++ MineField.java
@@ -83,19 +83,10 @@ class MineField{
 				return '?';
 			}
 		}
-		switch(count){
-		case 0:return '0';
-		case 1:return '1';
-		case 2:return '2';
-		case 3:return '3';
-		case 4:return '4';
-		case 5:return '5';
-		case 6:return '6';
-		case 7:return '7';
-		case 8:return '8';
 
+		return (count >= 0 && count <= 8) ? (char)count : 'x';
 
-		default:return 'X';
+		
 		}
 	}
 	public boolean getBoom(){
